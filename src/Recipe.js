c9612f9a34627e03291f9dbbd5edd515e15916f7
Anh Nguyen <anhnguyenis@gmail.com>
2020-01-13 01:21:26 +0000

    render recipe response

---
+++ src/Recipe.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import style from './recipe.module.css';
+
+const Recipe = ({title, image, time, cuisine, servings, wines, ingredients, instructions, myIngredients, extraIngredients}) => {
+    return(
+        <div className={style.recipe}>
+            <h1>{title}</h1>
+            <ol>
+                Make this with:
+                {/* {instructions[0].map(steps => steps.map(instruction) => 
+                <li key={instruction}>{instruction}</li>
+                )} */}
+                {/* {myIngredients.map(ingredient =>
+                   <li key={ingredient.name}>{ingredient.name}</li> 
+                )}
+                {extraIngredients.map(ingredient =>
+                   <li key={ingredient.name}>{ingredient.name}</li> 
+                )} */}
+            </ol>
+            <img className={style.image} src={image} alt=""/>
+            <ol>Ready in {time} minutes.</ol>
+            <ol>Serves up to {servings}</ol>
+            
+            <ul>{cuisine.map((cuisine,index) =>
+                    <li key={index}>{cuisine}</li>
+                )}
+            </ul>
+        </div>
+    );
+}
+
+export default Recipe;
